bab89d0ed561a099c901a5ccfe82ba2dd6025134
Scott Mitchell <mitchsco@sonoma.edu>
2016-09-21 13:53:57 -0700

    Updated with maven project in eclipse

---
+++ GMCApp/src/main/java/com/cs370/alexa/GMCApp/GMC.java
@@ -0,0 +1,13 @@
+package com.cs370.alexa.GMCApp;
+
+/**
+ * Hello world!
+ *
+ */
+public class GMC 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
